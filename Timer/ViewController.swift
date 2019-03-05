39614050652c2e7beeabc7c47b4e3f36fedd0307
Svoboda <sashasbox.forjob@gmail.com>
2019-03-05 10:30:53 +0100

    1) Added labels "m" - for minutes and "s" - for seconds. 2) Added space btwn timerLabel and buttons. 3) Added: After an hour the counting automatically stops and a text about passed time appears.

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -15,30 +15,60 @@ class ViewController: UIViewController {
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var spaceForTextLabel: UILabel!
     
     
     @IBAction func startPressed(_ sender: Any) {
+        
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerUpdate), userInfo: NSDate(), repeats: true)
         
         startButton.isHidden = true
         stopButton.isHidden = false
+        
+        spaceForTextLabel.text = ""
     }
     
     @IBAction func stopPressed(_ sender: Any) {
+        
         timer.invalidate()
         
         startButton.isHidden = false
         stopButton.isHidden = true
+        
     }
     
     @objc func timerUpdate () {
-        let passed = -(self.timer.userInfo as! NSDate).timeIntervalSinceNow
+         let passed = -(self.timer.userInfo as! NSDate).timeIntervalSinceNow
         
         if passed < 60 {
-            timerLabel.text = String(format: "%.0f", passed)
+            print("sec counting started")
+            timerLabel.text = passed < 10 ? String(format: "0%.0f s", passed) : String(format: "%.0f s", passed)
         } else {
-            timerLabel.text = String(format: "%.0f : %.0f", passed/60, passed.truncatingRemainder(dividingBy: 60))
+            print("min counting started")
+            timerLabel.text = String(format: "\(format(value: Int(passed/60))) m : \(format(value: Int(passed.truncatingRemainder(dividingBy: 60)))) s", passed/60, passed.truncatingRemainder(dividingBy: 60))
+            
         }
+        
+        if passed >= 3600 {
+            print("more than 3600")
+            stopPressed(_ : true)
+            spaceForTextLabel.text = """
+            More than an hour has passed.
+            I am tired of counting.
+            Come later.
+            """
+        }
+    }
+    
+    func format(value: Int) -> String {
+        let extraZero = "0%.0f"
+        let noZero = "%.0f"
+        if value < 10 {
+            return extraZero
+        } else {
+            return noZero
+        }
+        
     }
     
     
